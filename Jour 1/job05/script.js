61912838ff32a4e28d756435d7ed131e99a1559b
scott <scott.allo@laplateforme.io>
2025-03-10 16:42:54 +0100

    Job05 du jour01, utilisation d'une fonction constante qui defile un tableau des jours de la semaine et l'affiche dans la console

---
+++ Jour 1/job05/script.js
@@ -0,0 +1,11 @@
+function afficherJoursSemaines() {
+
+    const joursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+
+    for (let i = 0; i < joursSemaines.length; i++) {
+        console.log(joursSemaines[i]);
+    }
+}
+
+afficherJoursSemaines();
